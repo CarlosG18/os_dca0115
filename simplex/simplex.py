ef278c95817a2bf76cde0d6cc422ac1977d0d62b
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-09-10 22:46:22 -0300

    add method simplex - faltar fezer testes de verificação

--- simplex/simplex.py
+++ simplex/simplex.py
@@ -1,49 +1,44 @@
 import numpy as np
 
+def isNegative(array):
+  return np.any(array < 0)
+
 matriz_base = np.array([
-    [1, -10, -15, -20, 0, 0, 0, 0, 0],
     [0, 2, 3, 1, 1, 0, 0, 0, 100],
     [0, 1, 1, 2, 0, 1, 0, 0, 120],
     [0, 1, 2, 3, 0, 0, 1, 0, 176],
     [0, 1, 2, 1, 0, 0, 0, 1, 132],
   ])
-  
-vetorB = np.array([0, 100, 120, 176, 132])
-vetor_fatorP = np.zeros(5)
-candidato_base = {
-  "value": 0,
-  "index": 0,
-  "line": 0,
-}
-index_base = 0
 
-for i in matriz_base[0, :]:
-  if i < candidato_base["value"]:
-    candidato_base["value"] = i
-    candidato_base["index"] = index_base
-  index_base += 1
- 
-cont = 0
-for i in matriz_base[:, 8]:
-    vetor_fatorP[cont] = (i/matriz_base[cont, candidato_base["index"]])
-    cont += 1
+FOB = np.array([1, -10, -15, -20, 0, 0, 0, 0, 0])
 
+matriz_base = np.array([
+    
+])
 
-#primeira lapada
-matriz_base[3, :] = matriz_base[3, :] / 3
+(lin, col) = matriz_base.shape
 
-matriz_base[0, :] = matriz_base[3, :] * 20 + matriz_base[0, :]
-matriz_base[1, :] = matriz_base[3, :] * -1 + matriz_base[1, :]
-matriz_base[2, :] = matriz_base[3, :] * -2 + matriz_base[2, :]
-matriz_base[4, :] = matriz_base[3, :] * -1 + matriz_base[4, :]
+while(isNegative(FOB)):
+  #encontrar a coluna base
+  col_base = np.where(FOB == FOB.min())
+  print(f'coluna base = {col_base[0]}')
 
-linha_base = 1
-col_base = 2
-matriz_base[linha_base, :] = matriz_base[linha_base, :] / matriz_base[linha_base,col_base]
+  #encontrar a linha base
+  vetor_fatorP = np.zeros(lin-1)
+  for i in range(lin-1):
+    vetor_fatorP[i] = matriz_base[i,col-1] / matriz_base[i,col_base[0][0]]
+  linha_base = np.where(vetor_fatorP == vetor_fatorP.min())
+  print(f'linha base = {linha_base[0][0]}')
 
-matriz_base[0, :] = matriz_base[linha_base, :] *  + matriz_base[0, :]
-matriz_base[1, :] = matriz_base[linha_base, :] * -1 + matriz_base[1, :]
-matriz_base[2, :] = matriz_base[linha_base, :] * -2 + matriz_base[2, :]
-matriz_base[4, :] = matriz_base[linha_base, :] * -1 + matriz_base[4, :]
+  #fazendo a eliminação de gaus-jordan
+  matriz_base[linha_base[0][0], :] = matriz_base[linha_base[0][0], :] / matriz_base[linha_base[0][0], col_base[0][0]]
+  FOB = matriz_base[linha_base[0][0], :] * -FOB[col_base[0][0]] + FOB
 
-print(matriz_base)
+  print(f'linha {linha_base[0][0]} pivotizada \n {matriz_base}')
+  for i in range(lin):
+    if i != linha_base[0][0]:
+      matriz_base[i, :] = matriz_base[linha_base[0][0], :] * -matriz_base[i, col_base[0][0]] + matriz_base[i, :]  
+
+  print(f'função objetivo {FOB}')
+  print(isNegative(FOB))
+  print(f'depois da primeira lapada \n {matriz_base}')
